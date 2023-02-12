9626af3d467a1c13a1c531d7817f02b125bc6639
Blueshadow58 <gamonalfranco@gmail.com>
2023-02-12 18:31:23 -0300

    netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
+// import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/edge-functions';
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare()
+  adapter: netlify()
 });
